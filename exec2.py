eb9c02d8e7f2f8160b03661a238cd7b9c4a8fe12
Beatriz Cristina <beatriz.luiz@unisul.br>
2019-11-26 14:44:35 -0300

    Update exec2.py

--- exec2.py
+++ exec2.py
@@ -1,4 +1,4 @@
 #Calcular raio de um circulo A = π . r2
 raio = float(input("Digite o raio de um circulo: "))
-area = 3.14 *(raio*raio)
+area = 3.14 *(raio**2)
 print(f"A área do circulo com o raio {raio} é {area}.")
