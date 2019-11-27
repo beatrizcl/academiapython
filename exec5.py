b1451350a8fdee64b4876a543568c9f202c787bf
Beatriz Cristina <beatriz.luiz@unisul.br>
2019-11-27 11:09:45 -0300

    upload exec5

---
+++ exec5.py
@@ -0,0 +1,9 @@
+#media ponderada
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("digite o segundo valor: "))
+valor3 = float(input("Digite o terceiro valor: "))
+
+result = (((valor1*2)+(valor2*3)+(valor3*5))/10)
+
+print(f"A média ponderada é {result}")
+
