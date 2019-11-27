179733c85886561a05d40ace246df99c10d2db02
Beatriz Cristina <beatriz.luiz@unisul.br>
2019-11-27 15:32:58 -0300

    upload lista_exec2

---
+++ lista_exec2.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite a nota 1 do aluno: "))
+nota2 = float(input("Digite a nota 2 do aluno: "))
+nota3 = float(input("Digite a nota 3 do aluno: "))
+nota4 = float(input("Digite a nota 4 do aluno: "))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+print(f"A média do aluno é {media}.")
