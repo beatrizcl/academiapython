565193bbdd2fc54fac850cd7e439ab55dab9057f
Beatriz Cristina <beatriz.luiz@unisul.br>
2019-11-27 16:02:54 -0300

    upload lista_exec5

---
+++ lista_exec5.py
@@ -0,0 +1,6 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = ((nota1*2) + (nota2*3) + (nota3*5))/10
+print(f"A media do aluno é {media}")
