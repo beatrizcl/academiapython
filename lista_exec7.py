9efe4ce09d6684f808c0571525177a5dc7e80393
Beatriz Cristina <beatriz.luiz@unisul.br>
2019-11-28 08:29:14 -0300

    Removendo float desnecessário

--- lista_exec7.py
+++ lista_exec7.py
@@ -1,5 +1,5 @@
 celcius = float(input("Digite a temperatura em Celcius: "))
 
-fahrenheit = float(celcius*1.8+32)
+fahrenheit = (celcius*1.8+32)
 
 print(f"A temperatura em Fahrenheit é {fahrenheit}")
