37a99d534ac698c0c2cf7461a3549712c7396254
Beatriz Cristina <beatriz.luiz@unisul.br>
2019-11-28 10:20:58 -0300

    Incluindo a quantidade de dias para conversão

--- lista_exec10.py
+++ lista_exec10.py
@@ -1,13 +1,15 @@
+dia = int(input("Digite a quantidade de dias(apenas números): "))
 hora, minuto, segundo = input("Digite a quantidade de horas, minutos e segundos no formato 00:00:00: ").split(':')
 print(hora, minuto, segundo)
 hora = int(hora)
 minuto = int(minuto)
 segundo = int(segundo)
 
+dia = dia*86400
 hora = hora*3600
 print(hora)
 minuto = minuto*60
 print(minuto)
 print(segundo)
-result = hora+minuto+segundo
+result = dia+hora+minuto+segundo
 print(f"A correção corresponde a {result} segundos.")
