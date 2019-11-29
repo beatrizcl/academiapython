52b132f713aa97366e878ec09dff1950bc53bef9
Beatriz Cristina <beatriz.luiz@unisul.br>
2019-11-29 15:26:41 -0300

    Delete modulo 2

--- modulo2_exec2.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Digite um número: "))
-
-if num >=  0 and num <= 50:
-    print("Intervalo [0,50]")
-elif num >= 51 and num <= 100:
-    print("Intervalo [51,100]")
-else: 
-    print("Fora do intervalo")
