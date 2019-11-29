d48e40055cbc884530d47a9b2b6d2adf78edd284
Beatriz Cristina <beatriz.luiz@unisul.br>
2019-11-29 15:25:48 -0300

    upload exec2

---
+++ modulo2_exec2.py
@@ -0,0 +1,8 @@
+num = int(input("Digite um número: "))
+
+if num >=  0 and num <= 50:
+    print("Intervalo [0,50]")
+elif num >= 51 and num <= 100:
+    print("Intervalo [51,100]")
+else: 
+    print("Fora do intervalo")
